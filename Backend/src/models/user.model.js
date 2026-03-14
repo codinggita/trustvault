da9ae849f982647a2e9e6cd70e78e155fa6d9802
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2026-03-14 23:06:34 +0530

    Adding Middele ware and email service for this

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,50 +1,57 @@
 const mongoose = require("mongoose")
 const bcrypt = require("bcryptjs")
 
-const userSchema =new mongoose.Schema({
-    email:{
-        type:String,
-        requried:[true,"Email is required for creating a user"],
-        trim:true,
-        lowercase:true,
-        match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-             "InValid Email Address"
-            ],
-            unique:[true, "Email already Exist"]
-    },
-    name:{
+
+const userSchema = new mongoose.Schema({
+    email: {
         type: String,
-        required:[true, "Name is required For creating an Account" ]
+        required: [ true, "Email is required for creating a user" ],
+        trim: true,
+        lowercase: true,
+        match: [ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "Invalid Email address" ],
+        unique: [ true, "Email already exists." ]
     },
-    password:{
-        type:String,
-        required:[true,"Password is required for creating an account"],
-        minlength: [6,"Password Must contains 6 character"],
-        select:false
+    name: {
+        type: String,
+        required: [ true, "Name is required for creating an account" ]
+    },
+    password: {
+        type: String,
+        required: [ true, "Password is required for creating an account" ],
+        minlength: [ 6, "password should contain more than 6 character" ],
+        select: false
+    },
+    systemUser: {
+        type: Boolean,
+        default: false,
+        immutable: true,
+        select: false
     }
-    
-},
-{
-    timestamps:true
+}, {
+    timestamps: true
 })
 
-userSchema.pre("save",async function(next) {
-    if(!this.isModifed("password")){
-        return next()
+userSchema.pre("save", async function () {
+    if (!this.isModified("password")) {
+        return
     }
 
-    const hash = await bcrypt.hash(this.password,10)
+    const hash = await bcrypt.hash(this.password, 10)
     this.password = hash
-    return next()
+
+    return
 
 })
 
-userSchema.method.camparePassword = async function (password) {
+userSchema.methods.comparePassword = async function (password) {
 
-    return await bcrypt.compare(password,this.password)
+    console.log(password, this.password)
+
+    return await bcrypt.compare(password, this.password)
 
 }
 
-const userModel = mongoose.model("user",userSchema)
+
+const userModel = mongoose.model("user", userSchema)
 
 module.exports = userModel
