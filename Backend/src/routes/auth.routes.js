ae58f9c06aad3be8afc2463ea9683b49613a3e03
DashratRajpurohit <dashrat.rajpurohit.cg@gamil.com>
2026-03-14 22:30:09 +0530

    For user SinUp all process is done

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const express = require("experss")
+const authController = require()
 
 const router = express.Router()
-
+                                                                                                                                 
+router.post("/register", authController.userRegisterController)
 
 module.exports = router
